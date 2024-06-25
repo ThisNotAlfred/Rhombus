e6b8d2f5be2b9375aaa4d424e43064456b4337d2
NotAlfred <mohammadalitalebi2001@gmail.com>
2024-06-25 11:37:51 +0330

    added instructions

---
+++ source/include/instruction.hpp
@@ -0,0 +1,38 @@
+#include <variant>
+
+using Register = std::size_t;
+
+struct InstNoReg {
+    enum { NOP } instruction;
+};
+
+struct InstOneReg {
+    enum {
+        JMPE,
+        JMPB,
+        JMPS,
+        PRINT,
+        SCAN,
+    } instruction;
+
+    Register dest;
+};
+struct InstTwoReg {
+    enum {
+        MOV,
+        SHR,
+        SHL,
+        ADD,
+        SUB,
+        XOR,
+        OR,
+        AND,
+        CMPR,
+    } instruction;
+
+    Register source;
+    Register dest;
+};
+
+using Instruction = std::variant<InstNoReg, InstOneReg, InstTwoReg>;
+
