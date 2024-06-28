674afa3ea93a7e97ccdde524fc143ca50260fa01
NotAlfred <mohammadalitalebi2001@gmail.com>
2024-06-28 08:03:48 +0330

    tokenizer now is more generic

--- source/tokenizer.cpp
+++ source/tokenizer.cpp
@@ -1,32 +1,38 @@
 #include "tokenizer.hpp"
 
 #include <algorithm>
+#include <print>
 
 auto
 Tokenizer::tokenize() -> std::vector<std::string>
 {
     std::vector<std::string> tokens;
 
-    for (const auto& chr : this->content) {
+    for (; this->current_header < this->content.size();) {
         // skipping whitespaces
-        if (chr == ' ' || chr == '\n' || chr == '\r' || chr == '\t' || chr == '\0' || chr == ',') {
+        if (this->content[this->current_header] == ' ' ||
+            this->content[this->current_header] == '\n' ||
+            this->content[this->current_header] == '\r' ||
+            this->content[this->current_header] == '\t') {
+            this->current_header++;
             continue;
         }
 
-        if (chr == '[') {
+        if (this->content[this->current_header] == ';') {
+            skip_comment();
+        }
+
+        if (this->content[this->current_header] == '[') {
             tokens.push_back(this->tokenize_brackets());
         }
 
         // reading whole tokens of instructions
         tokens.push_back(this->tokenize_instruction());
-
-        // moving header one forward
-        ++this->current_header;
     }
 
-    tokens.erase(std::remove_if(tokens.begin(), tokens.end(),
-                                [](auto& element) { return element.size() < 1; }),
-                 tokens.end());
+    tokens.erase(
+        std::remove_if(tokens.begin(), tokens.end(), [](auto& element) { return element.empty(); }),
+        tokens.end());
 
     return tokens;
 }
@@ -37,15 +43,14 @@ Tokenizer::tokenize_brackets() -> std::string
     std::vector<char> temp_token = {};
 
     for (auto i = this->current_header; i < this->content.size(); ++i) {
-        if (this->content[i] == ' ' || this->content[i] == '\n' || this->content[i] == '\t' ||
-            this->content[i] == '\0' || this->content[i] == ',' || this->content[i] == '\r' ||
-            this->content[i] == ';' || this->content[i] == '[' || this->content[i] == ']') {
+        this->current_header++;
+
+        if (this->content[i] == '\n' || this->content[i] == '\r' || this->content[i] == ',' ||
+            this->content[i] == ';') {
             break;
         }
 
         temp_token.push_back(this->content[i]);
-
-        ++this->current_header;
     }
 
     auto token = std::string { temp_token.begin(), temp_token.end() };
@@ -60,6 +65,9 @@ Tokenizer::tokenize_instruction() -> std::string
     std::vector<char> temp_token = {};
 
     for (auto i = this->current_header; i < this->content.size(); ++i) {
+        this->current_header++;
+
+
         if (this->content[i] == ' ' || this->content[i] == '\n' || this->content[i] == '\t' ||
             this->content[i] == '\0' || this->content[i] == ',' || this->content[i] == '\r' ||
             this->content[i] == ';' || this->content[i] == '[') {
@@ -67,16 +75,23 @@ Tokenizer::tokenize_instruction() -> std::string
         }
 
         temp_token.push_back(this->content[i]);
-
-        ++this->current_header;
     }
 
     auto token = std::string { temp_token.begin(), temp_token.end() };
 
-    // mkaing the token lowercase
-    // TODO: find a more efficient way. this doesn't seem good!
     std::transform(token.begin(), token.end(), token.begin(),
                    [](auto chr) { return std::tolower(chr); });
 
     return token;
+}
+
+auto
+Tokenizer::skip_comment() -> void
+{
+    for (auto i = this->current_header; i < this->content.size(); ++i) {
+        this->current_header++;
+        if (this->content[i] == '\n') {
+            break;
+        }
+    }
 }
